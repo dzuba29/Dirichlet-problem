6d75228a886811baac208a8d1b9e66bc00a1ac56
dzuba <chepik29rus@gmail.com>
2018-03-21 16:51:03 +0300

    fix sizes

--- run_to_csv.sh
+++ run_to_csv.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-
 touch benchmark.csv
 
-num_for_repeat=10
+num_for_repeat=5
 
 echo "Threads,Dimension,MultTime,RunTime" > benchmark.csv
 
 for omp_threads in 1 2 3 4 
 do
 	export OMP_NUM_THREADS=$omp_threads
-	for dim in 500 700 900 1100 1300 1500
+	for dim in 900 1100 1300 1500
 	do
 		for ((i=0; i < $num_for_repeat; i++))
 		do
