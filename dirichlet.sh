f2fd2d6c02658b68b410379a6c717714cd712b31
Dzuba <chepik29rus@gmail.com>
2018-05-20 20:27:54 +0300

    Fix dirich.sh

--- dirichlet.sh
+++ dirichlet.sh
@@ -1,47 +1,16 @@
 #!/bin/bash
 
-touch surface.csv
-touch surface2.csv
-touch surface4.csv
-
-
 num_for_repeat=1
 
-
-for omp_threads in 1 
+for omp_threads in 1 2 3 4
 do
 	export OMP_NUM_THREADS=$omp_threads
 	for iters in 999
 	do
 		for ((i=0; i < $num_for_repeat; i++))
 		do
-		./main/main $iters > surface.csv
-		done
-	done
-done
-
-
-for omp_threads in 2
-do
-	export OMP_NUM_THREADS=$omp_threads
-	for iters in 999
-	do
-		for ((i=0; i < $num_for_repeat; i++))
-		do
-		./main/main $iters > surface2.csv
-		done
-	done
-done
-
-
-for omp_threads in 4
-do
-	export OMP_NUM_THREADS=$omp_threads
-	for iters in 999
-	do
-		for ((i=0; i < $num_for_repeat; i++))
-		do
-		./main/main $iters > surface4.csv
+		./main/main $iters > surface${omp_threads}.csv
+		
 		done
 	done
 done
--- surf_plot.py
+++ surf_plot.py
@@ -2,23 +2,37 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import glob
 
-data = pd.read_csv('surface4.csv',header=None)
+allFiles = glob.glob("*.csv")
+
+data=[]
+time=[]
+
+for file in allFiles:
+	temp_data=pd.read_csv(file,header=None)
+	temp_time=temp_data[len(temp_data)][0]
+	temp_data=temp_data.drop(len(temp_data),1)
+	data.append(temp_data)
+	time.append(temp_time)
 
 
-
-time=data[len(data)][0] #get time
-data = data.drop(len(data), 1) #drop last 'time' column
+fig = plt.figure(figsize=(8,12))
 
 
-Z=np.array(data)
-X = np.arange(0, data.shape[0], 1)
-Y = np.arange(0,data.shape[1], 1)
+for ind,item in enumerate(data):
 
-X2D,Y2D = np.meshgrid(X,Y)
-fig = plt.figure()
-ax = Axes3D(fig)
+	ax = fig.add_subplot(len(data),len(data)/2,ind+1, projection='3d')
+	Z=np.array(item)
+	X = np.arange(0,item.shape[0], 1)
+	Y = np.arange(0,item.shape[1], 1)
+	Xgrid,Ygrid = np.meshgrid(X,Y)
 
-ax.plot_surface(X2D,Y2D, Z)
-plt.title("Surface\n"+"Time:"+str(time))
+	ax.plot_surface(Xgrid,Ygrid, Z)
+	ax.set_xlabel('Z')
+	ax.set_ylabel('X')
+	ax.set_zlabel('Y')
+
+plt.tight_layout()
+plt.savefig('yolo.pdf')
 plt.show()
